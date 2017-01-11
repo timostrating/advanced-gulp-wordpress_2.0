c85b01a8b6a027bd0e67de568f0a9295464ea28d
Timo Strating <timostrating@home.nl>
2017-01-11 01:15:54 +0100

    bootstrap support header

--- header.php
+++ header.php
@@ -18,10 +18,34 @@
 
 	<header class="aa_navigation">
 
-		<nav class="aa_nav">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				  <a class="navbar-brand" href="#"></a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+						<?php wp_nav_menu( array(
+							'theme_location' => 'primary',
+							'menu_id' => 'primary-menu',
+							'depth' => 2,
+							'container' => false,
+							'menu_class' => 'nav navbar-nav navbar-left',
+							'walker' => new wp_bootstrap_navwalker())
+						);  ?>
+						
+				</div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
 		</nav>
-		<!-- /.aa_nav -->
 
 	</header>
 	<!-- /.aa_navigation -->
